a42403b820f0413f14453618db7fadb88e4892a4
Nikhil Kothare <nikhilkothare712@gmail.com>
2025-02-16 01:46:58 +0530

    landing page, signup added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
+// import SignUp from "./components/signUp/SignUp.jsx";
 
+import Navbar from "./components/landingPage/Navbar";
+import HeroSection from "./components/landingPage/HeroSection";
+import FeatureSection from "./components/landingPage/FeatureSection";
+import Workflow from "./components/landingPage/Workflow";
+import Footer from "./components/landingPage/Footer";
+import Pricing from "./components/landingPage/Pricing";
+import Testimonials from "./components/landingPage/Testimonials";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Navbar />
 
-export default App
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import SignUp from "./Components/SignUp/SignUp.jsx";
+import Login from "./Components/SignUp/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<App />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+);
+
+{
+  /* <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} loader={githubInfo} /> */
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
